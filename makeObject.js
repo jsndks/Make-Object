ca36f12c6631051a6309b9cb4a730a19b44d824c
Jason Dicks <jtd361@gmail.com>
2014-07-18 17:11:45 -0500

    update plugin code

--- makeObject.js
+++ makeObject.js
@@ -11,6 +11,33 @@ var MODULE = (function () {
     var Stream = require("stream").Stream;
     var path = require("path");
     var mkdirp = require('mkdirp');
+    var Handlebars = require('handlebars');
+    var Promise = require('promise');
+
+    // ----------------------------------------------------------------
+    // Setup Variables
+    // ----------------------------------------------------------------
+    var CONFIG = {
+        EXT: 'scss',
+        PREFIX: '_',
+        BASE_URI: 'assets/styles/',
+        DIR_OBJECTS: 'objects/',
+        TEMPLATE: 'objectTemplate.hbs'
+    };
+
+    var knownOpts = {
+        'helpers': Boolean,
+        'breakpoints': [String, Array]
+    };
+
+    var shortHands = {
+        'h': ['--helpers'],
+        'b': ['--breakpoints']
+    };
+
+    var parsed = nopt(knownOpts, shortHands, process.argv);
+    var args = parsed.argv.remain;
+    var flagArgs = parsed;
 
     /**
      * A utility for creating files in an opinionated directory structure
@@ -32,79 +59,97 @@ var MODULE = (function () {
      * @private
      */
     proto._init = function() {
-        console.log('init');
+        this._createChildren()
+            ._createDir();
 
         return this;
     }
 
-    // ----------------------------------------------------------------
-    // Setup Variables
-    // ----------------------------------------------------------------
-    var CONFIG = {
-        EXT: 'scss',
-        PREFIX: '_',
-        BASE_URI: 'assets/styles/',
-        DIR_OBJECTS: 'objects'
-    };
-
-    var knownOpts = {
-        'helpers': Boolean,
-        'breakpoints': [String, Array]
-    };
-
-    var shortHands = {
-        'h': ['--helpers'],
-        'b': ['--breakpoints']
-    };
-
-    var parsed = nopt(knownOpts, shortHands, process.argv);
-    var args = parsed.argv.remain;
-    var flagArgs = parsed;
-
-    console.log(parsed);
-
-    var objName = args[0];
-    var objPath = CONFIG.BASE_URI + CONFIG.DIR_OBJECTS + args[0];
-    var baseFileName = CONFIG.PREFIX + args[0] + '.' + CONFIG.EXT;
-    var objectEndpoint = objPath + baseFileName;
-
-    // ----------------------------------------------------------------
-    // Create the object Badge
-    // ----------------------------------------------------------------
-    var badge = '/* ---------------------------------------------------------------------';
-    badge += '\n';
-    badge += objName;
-    badge += '\n';
-    badge += '------------------------------------------------------------------------ */';
-
-    // ----------------------------------------------------------------
-    // Create the object directory
-    // ----------------------------------------------------------------
-    function createDir() {
-        mkdirp(objPath, function(err) {
-            console.log('dir ' + args[0] + ' was created');
-            createFile();
+    /**
+     * templates out the base object
+     *
+     * @method _templateObject
+     * @public
+     */
+    proto._getBadge = function(objectName) {
+        var templatePromise = new Promise(function(resolve, reject) {
+            fs.readFile(CONFIG.TEMPLATE, 'utf8', function(err, res) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(res);
+                }
+            });
         });
-    }
+
+        return templatePromise.done(this._templateFile.bind(this, objectName));
+
+        // templatePromise.done(function(source) {
+        //     var template = Handlebars.compile(source);
+        //     var data = { "title": objectName };
+        //     console.log(template(data));
+        // });
+    };
+
+    /**
+     * Templates out a file
+     *
+     * @method _templateFile
+     * @public
+     */
+    proto._templateFile = function(objectName, source) {
+        var template = Handlebars.compile(source);
+        var data = { "title": objectName };
+        return template(data);
+    };
+
+    /**
+     * Create any child objects or references
+     * Should only be run on initialization of the view.
+     *
+     * @method _createChildren
+     * @returns {makeObject}
+     * @private
+     */
+    proto._createChildren = function() {
+        this.objectProps = {
+            name: args[0],
+            path: CONFIG.BASE_URI + CONFIG.DIR_OBJECTS + args[0],
+            fileName: CONFIG.PREFIX + args[0] + '.' + CONFIG.EXT
+        };
+
+        return this;
+    };
 
     // ----------------------------------------------------------------
     // Create the object directory
     // ----------------------------------------------------------------
-    function createFile() {
-        fs.writeFile(objectEndpoint, badge);
-        if (flagArgs.responsive) {
-            var breakpoints = flagArgs.responsive.split(',');
-            var i = 0;
-            var l = breakpoints.length;
-            for (; i < l; i++) {
-                var fileName = objPath + '/' + '_' + args[0] + '_' + breakpoints[i] + '.' + STYLE_EXT;
-                var fileName = fileName.replace(/ /g,'');
-                fs.writeFile(fileName, badge);
-            }
-        }
-    }
+    proto._createDir = function() {
+        var self = this;
+        mkdirp(this.objectProps.path, function(err) {
+            console.log('dir ' + args[0] + ' was created');
+            self._createFile();
+        });
+    };
 
-    var importString = '';
+    // ----------------------------------------------------------------
+    // Create the object directory
+    // ----------------------------------------------------------------
+    proto._createFile = function() {
+        console.log(this._getBadge(this.objectProps.name));
+
+        // fs.writeFile(this.objectProps.path + this.objectProps.filename, this._getBadge(this.objectProps.name));
+        // if (flagArgs.responsive) {
+        //     var breakpoints = flagArgs.responsive.split(',');
+        //     var i = 0;
+        //     var l = breakpoints.length;
+        //     for (; i < l; i++) {
+        //         var fileName = objPath + '/' + '_' + args[0] + '_' + breakpoints[i] + '.' + STYLE_EXT;
+        //         var fileName = fileName.replace(/ /g,'');
+        //         fs.writeFile(fileName, badge);
+        //     }
+        // }
+    }
 
     // ----------------------------------------------------------------
     // Create import files
@@ -113,7 +158,7 @@ var MODULE = (function () {
         // var importString += '@import' + ' ("' $filepath '")';
     // endforeach
     // fs.writeFile(objPath + '/' + baseFileName, importString);
-    function generateImports() {
+    proto.generateImports = function() {
         processDir(CONFIG.BASE_URI + CONFIG.DIR_OBJECTS, function(arg1, dir) {
             processFile(dir, writeToImportFile(err, filename));
         });
@@ -121,12 +166,12 @@ var MODULE = (function () {
         setTimeout(function() {
             console.log(importString);
         }, 1000);
-    }
+    };
 
     // ----------------------------------------------------------------
     // Utility for preforming an action on each file
     // ----------------------------------------------------------------
-    function processFile(dir, action) {
+    proto.processFile = function(dir, action) {
         // Assert that it's a function
         if (typeof action !== "function") {
             action = function (error, file) { };
@@ -158,12 +203,12 @@ var MODULE = (function () {
                 });
             });
         });
-    }
+    };
 
     // ----------------------------------------------------------------
     // Utility for preforming an action on each dir
     // ----------------------------------------------------------------
-    function processDir(dir, action) {
+    proto.processDir = function(dir, action) {
         // Assert that it's a function
         if (typeof action !== "function") {
             action = function (error, file) { };
@@ -192,15 +237,15 @@ var MODULE = (function () {
                 });
             });
         });
-    }
+    };
 
     // ----------------------------------------------------------------
     //
     // ----------------------------------------------------------------
-    function writeToImportFile(err, fileName) {
+    proto.writeToImportFile = function(err, fileName) {
         importString += '@import' + ' ("' + fileName + '");';
         importString += '\n';
-    }
+    };
 
     // ----------------------------------------------------------------
     // Kick-off functions
